cf065b644690d0e5cd0c8a4e9b0cab1620cf50ce
ゆき / yuki920 <109013663+yuki-920@users.noreply.github.com>
2026-02-06 21:36:56 +0900

    Update Statsify.java

--- src/main/java/com/yuki920/statsify/Statsify.java
+++ src/main/java/com/yuki920/statsify/Statsify.java
@@ -127,31 +127,34 @@ public class Statsify {
 
         for (NetworkPlayerInfo playerInfo : playerInfoList) {
             if (playerInfo == null || playerInfo.getGameProfile() == null) continue;
-
+        
             String playerName = playerInfo.getGameProfile().getName();
             List<String> suffixv = playerSuffixes.get(playerName);
-
+        
+            String[] tabData = getTabDisplayName2(playerName);
+            String team = tabData[0], name = tabData[1], suffix = tabData[2];
+            String teamColor = team.length() >= 2 ? team.substring(0, 2) : "";
+        
+            String newDisplayName;
+        
             if (suffixv != null && suffixv.size() >= 2) {
-                String[] tabData = getTabDisplayName2(playerName);
-                String team = tabData[0], name = tabData[1], suffix = tabData[2];
-
-                if (!name.endsWith("\u30fb" + suffixv.get(1))) {
-                    String teamColor = team.length() >= 2 ? team.substring(0, 2) : "";
-                    String newDisplayName;
+                if (!name.endsWith(" \u00a78| " + suffixv.get(1))) {
                     if (tabFormat.equals("bracket_star_name_dot_fkdr")) {
-                        newDisplayName = team + "\u00a77[" + suffixv.get(0) + "\u00a77] " + teamColor + name + "\u30fb" + suffixv.get(1);
+                        newDisplayName = team + suffixv.get(0) + " " + teamColor + name + " \u00a78| " + suffixv.get(1);
                     } else if (tabFormat.equals("star_dot_name_dot_fkdr")) {
-                        newDisplayName = team + suffixv.get(0) + "\u30fb" + teamColor + name + "\u30fb" + suffixv.get(1);
+                        newDisplayName = team + suffixv.get(0) + " \u00a78| " + teamColor + name + " \u00a78| " + suffixv.get(1);
                     } else if (tabFormat.equals("name_dot_fkdr")) {
-                        newDisplayName = team + teamColor + name + "\u30fb" + suffixv.get(1);
+                        newDisplayName = team + teamColor + name + " \u00a78| " + suffixv.get(1);
                     } else {
-                        newDisplayName = team + "\u00a77[" + suffixv.get(0) + "\u00a77] " + teamColor + name + "\u30fb" + suffixv.get(1);
-
+                        newDisplayName = team + suffixv.get(0) + " " + teamColor + name + " \u00a78| " + suffixv.get(1);
                     }
-                    playerInfo.setDisplayName(new ChatComponentText(newDisplayName));
+                } else {
+                    continue;
                 }
+            } else {
+                newDisplayName = team + "\u00a78[\u00a75NICK\u00a78] " + teamColor + name;
             }
-        }
+            playerInfo.setDisplayName(new ChatComponentText(newDisplayName));
     }
 
     public static void sendToTablist(String playerName, String fkdr, String stars) {
@@ -802,208 +805,208 @@ public class Statsify {
         int Stars = Integer.parseInt(text);
         if (Stars < 100) {
             color = "\u00a77";
-            return color + text + "\u272b";
+            return color + "[" + text + "\u272b]";
         }
 
         if (Stars >= 100 && Stars < 200) {
-            color = "\u00A7f";
-            return color + text + "\u272b";
+            color = "\u00a7f";
+            return color + "[" + text + "\u272b]";
         }
         if (Stars >= 200 && Stars < 300) {
             color = "\u00a76";
-            return color + text + "\u272b";
+            return color + "[" + text + "\u272b]";
         }
         if (Stars >= 300 && Stars < 400) {
             color = "\u00a7b";
-            return color + text + "\u272b";
+            return color + "[" + text + "\u272b]";
         }
         if (Stars >= 400 && Stars < 500) {
             color = "\u00a72";
-            return color + text + "\u272b";
+            return color + "[" + text + "\u272b]";
         }
         if (Stars >= 500 && Stars < 600) {
             color = "\u00a73";
-            return color + text + "\u272b";
+            return color + "[" + text + "\u272b]";
         }
         if (Stars >= 600 && Stars < 700) {
             color = "\u00a74";
-            return color + text + "\u272b";
+            return color + "[" + text + "\u272b]";
         }
         if (Stars >= 700 && Stars < 800) {
             color = "\u00a7d";
-            return color + text + "\u272b";
+            return color + "[" + text + "\u272b]";
         }
         if (Stars >= 800 && Stars < 900) {
             color = "\u00a79";
-            return color + text + "\u272b";
+            return color + "[" + text + "\u272b]";
         }
         if (Stars >= 900 && Stars < 1000) {
             color = "\u00a75";
-            return color + text + "\u272b";
+            return color + "[" + text + "\u272b]";
         }
         if (Stars >= 1000 && Stars < 1100) {
             String[] digit = text.split("");
-            return "\u00a76" + digit[0] + "\u00a7e" + digit[1] + "\u00a7a" + digit[2] + "\u00a7b" + digit[3] + "\u00a7d" + "\u272b";
+            return "\u00a7c[\u00a76" + digit[0] + "\u00a7e" + digit[1] + "\u00a7a" + digit[2] + "\u00a7b" + digit[3] + "\u00a7d\u272b\u00a75]";
         }
         if (Stars >= 1100 && Stars < 1200) {
             String[] digit = text.split("");
-            return "\u00a7f" + digit[0] + digit[1] + digit[2] + digit[3] + "\u272a";
+            return "\u00a77[\u00a7f" + digit[0] + digit[1] + digit[2] + digit[3] + "\u272a\u00a77]";
         }
         if (Stars >= 1200 && Stars < 1300) {
             String[] digit = text.split("");
-            return "\u00a7e" + digit[0] + digit[1] + digit[2] + digit[3] + "\u00a76" + "\u272a";
+            return "\u00a77[\u00a7e" + digit[0] + digit[1] + digit[2] + digit[3] + "\u00a76\u272a\u00a77]";
         }
         if (Stars >= 1300 && Stars < 1400) {
             String[] digit = text.split("");
-            return "\u00a7b" + digit[0] + digit[1] + digit[2] + digit[3] + "\u00a73" + "\u272a";
+            return "\u00a77[\u00a7b" + digit[0] + digit[1] + digit[2] + digit[3] + "\u00a73\u272a\u00a77]";
         }
         if (Stars >= 1400 && Stars < 1500) {
             String[] digit = text.split("");
-            return "\u00a7a" + digit[0] + digit[1] + digit[2] + digit[3] + "\u00a72" + "\u272a";
+            return "\u00a77[\u00a7a" + digit[0] + digit[1] + digit[2] + digit[3] + "\u00a72\u272a\u00a77]";
         }
         if (Stars >= 1500 && Stars < 1600) {
             String[] digit = text.split("");
-            return "\u00a73" + digit[0] + digit[1] + digit[2] + digit[3] + "\u00a79" + "\u272a";
+            return "\u00a77[\u00a73" + digit[0] + digit[1] + digit[2] + digit[3] + "\u00a79\u272a\u00a77]";
         }
         if (Stars >= 1600 && Stars < 1700) {
             String[] digit = text.split("");
-            return "\u00a7c" + digit[0] + digit[1] + digit[2] + digit[3] + "\u00a74" + "\u272a";
+            return "\u00a77[\u00a7c" + digit[0] + digit[1] + digit[2] + digit[3] + "\u00a74\u272a\u00a77]";
         }
         if (Stars >= 1700 && Stars < 1800) {
             String[] digit = text.split("");
-            return "\u00a7d" + digit[0] + digit[1] + digit[2] + digit[3] + "\u00a75" + "\u272a";
+            return "\u00a77[\u00a7d" + digit[0] + digit[1] + digit[2] + digit[3] + "\u00a75\u272a\u00a77]";
         }
         if (Stars >= 1800 && Stars < 1900) {
             String[] digit = text.split("");
-            return "\u00a79" + digit[0] + digit[1] + digit[2] + digit[3] + "\u00a71" + "\u272a";
+            return "\u00a77[\u00a77[\u00a79" + digit[0] + digit[1] + digit[2] + digit[3] + "\u00a71\u272a\u00a77]";
         }
         if (Stars >= 1900 && Stars < 2000) {
             String[] digit = text.split("");
-            return "\u00a75" + digit[0] + digit[1] + digit[2] + digit[3] + "\u00a78" + "\u272a";
+            return "\u00a77[\u00a75" + digit[0] + digit[1] + digit[2] + digit[3] + "\u00a78\u272a\u00a77]";
         }
         if (Stars >= 2000 && Stars < 2100) {
             String[] digit = text.split("");
-            return "\u00a77" + digit[0] + "\u00a7f" + digit[1] + digit[2] + "\u00a77" + digit[3] + "\u269d";
+            return "\u00a78[\u00a77" + digit[0] + "\u00a7f" + digit[1] + digit[2] + "\u00a77" + digit[3] + "\u269d\u00a78]";
         }
         if (Stars >= 2100 && Stars < 2200) {
             String[] digit = text.split("");
-            return "\u00a7f" + digit[0] + "\u00a7e" + digit[1] + digit[2] + "\u00a76" + digit[3] + "\u269d";
+            return "\u00a7f[" + digit[0] + "\u00a7e" + digit[1] + digit[2] + "\u00a76" + digit[3] + "\u269d]";
         }
         if (Stars >= 2200 && Stars < 2300) {
             String[] digit = text.split("");
-            return "\u00a76" + digit[0] + "\u00a7f" + digit[1] + digit[2] + "\u00a7b" + digit[3] + "\u00a73" + "\u269d";
+            return "\u00a76[" + digit[0] + "\u00a7f" + digit[1] + digit[2] + "\u00a7b" + digit[3] + "\u00a73\u269d]";
         }
         if (Stars >= 2300 && Stars < 2400) {
             String[] digit = text.split("");
-            return "\u00a75" + digit[0] + "\u00a7d" + digit[1] + digit[2] + "\u00a76" + digit[3] + "\u00a7e" + "\u269d";
+            return "\u00a75[" + digit[0] + "\u00a7d" + digit[1] + digit[2] + "\u00a76" + digit[3] + "\u00a7e\u269d]";
         }
         if (Stars >= 2400 && Stars < 2500) {
             String[] digit = text.split("");
-            return "\u00a7b" + digit[0] + "\u00a7f" + digit[1] + digit[2] + "\u00a77" + digit[3] + "\u269d";
+            return "\u00a7b[" + digit[0] + "\u00a7f" + digit[1] + digit[2] + "\u00a77" + digit[3] + "\u269d\u00a78]";
         }
         if (Stars >= 2500 && Stars < 2600) {
             String[] digit = text.split("");
-            return "\u00a7f" + digit[0] + "\u00a7a" + digit[1] + digit[2] + "\u00a72" + digit[3] + "\u269d";
+            return "\u00a7f[" + digit[0] + "\u00a7a" + digit[1] + digit[2] + "\u00a72" + digit[3] + "\u269d]";
         }
         if (Stars >= 2600 && Stars < 2700) {
             String[] digit = text.split("");
-            return "\u00a74" + digit[0] + "\u00a7c" + digit[1] + digit[2] + "\u00a7d" + digit[3] + "\u269d";
+            return "\u00a74[" + digit[0] + "\u00a7c" + digit[1] + digit[2] + "\u00a7d" + digit[3] + "\u269d]";
         }
         if (Stars >= 2700 && Stars < 2800) {
             String[] digit = text.split("");
-            return "\u00a7e" + digit[0] + "\u00a7f" + digit[1] + digit[2] + "\u00a78" + digit[3] + "\u269d";
+            return "\u00a7e[" + digit[0] + "\u00a7f" + digit[1] + digit[2] + "\u00a78" + digit[3] + "\u269d]";
         }
         if (Stars >= 2800 && Stars < 2900) {
             String[] digit = text.split("");
-            return "\u00a7a" + digit[0] + "\u00a72" + digit[1] + digit[2] + "\u00a76" + digit[3] + "\u269d";
+            return "\u00a7a[" + digit[0] + "\u00a72" + digit[1] + digit[2] + "\u00a76" + digit[3] + "\u269d\u00a7e]";
         }
         if (Stars >= 2900 && Stars < 3000) {
             String[] digit = text.split("");
-            return "\u00a7b" + digit[0] + "\u00a73" + digit[1] + digit[2] + "\u00a79" + digit[3] + "\u269d";
+            return "\u00a7b[" + digit[0] + "\u00a73" + digit[1] + digit[2] + "\u00a79" + digit[3] + "\u269d\u00a71]";
         }
         if (Stars >= 3000 && Stars < 3100) {
             String[] digit = text.split("");
-            return "\u00a7e" + digit[0] + "\u00a76" + digit[1] + digit[2] + "\u00a7c" + digit[3] + "\u269d";
+            return "\u00a7e[" + digit[0] + "\u00a76" + digit[1] + digit[2] + "\u00a7c" + digit[3] + "\u269d\u00a74]";
         }
         if (Stars >= 3100 && Stars < 3200) {
             String[] digit = text.split("");
-            return "\u00a79" + digit[0] + "\u00a73" + digit[1] + digit[2] + "\u00a76" + digit[3] + "\u2725";
+            return "\u00a79[" + digit[0] + "\u00a73" + digit[1] + digit[2] + "\u00a76" + digit[3] + "\u2725\u00a7e]";
         }
         if (Stars >= 3200 && Stars < 3300) {
             String[] digit = text.split("");
-            return "\u00a74" + digit[0] + "\u00a77" + digit[1] + digit[2] + "\u00a74" + digit[3] + "\u00a7c" + "\u2725";
+            return "\u00a7c[\u00a74" + digit[0] + "\u00a77" + digit[1] + digit[2] + "\u00a74" + digit[3] + "\u00a7c\u2725]";
         }
         if (Stars >= 3300 && Stars < 3400) {
             String[] digit = text.split("");
-            return "\u00a79" + digit[0] + digit[1] + "\u00a7d" + digit[2] + "\u00a7c" + digit[3] + "\u2725";
+            return "\u00a79[" + digit[0] + digit[1] + "\u00a7d" + digit[2] + "\u00a7c" + digit[3] + "\u2725\u00a74]";
         }
         if (Stars >= 3400 && Stars < 3500) {
             String[] digit = text.split("");
-            return "\u00a7a" + digit[0] + "\u00a7d" + digit[1] + digit[2] + "\u00a75" + digit[3] + "\u2725";
+            return "\u00a72[\u00a7a" + digit[0] + "\u00a7d" + digit[1] + digit[2] + "\u00a75" + digit[3] + "\u2725\u00a72]";
         }
         if (Stars >= 3500 && Stars < 3600) {
             String[] digit = text.split("");
-            return "\u00a7c" + digit[0] + "\u00a74" + digit[1] + digit[2] + "\u00a72" + digit[3] + "\u00a7a" + "\u2725";
+            return "\u00a7c[" + digit[0] + "\u00a74" + digit[1] + digit[2] + "\u00a72" + digit[3] + "\u00a7a\u2725]";
         }
         if (Stars >= 3600 && Stars < 3700) {
             String[] digit = text.split("");
-            return "\u00a7a" + digit[0] + digit[1] + "\u00a7b" + digit[2] + "\u00a79" + digit[3] + "\u2725";
+            return "\u00a7a[" + digit[0] + digit[1] + "\u00a7b" + digit[2] + "\u00a79" + digit[3] + "\u2725\u00a71]";
         }
         if (Stars >= 3700 && Stars < 3800) {
             String[] digit = text.split("");
-            return "\u00a74" + digit[0] + "\u00a7c" + digit[1] + digit[2] + "\u00a7b" + digit[3] + "\u00a73" + "\u2725";
+            return "\u00a74[" + digit[0] + "\u00a7c" + digit[1] + digit[2] + "\u00a7b" + digit[3] + "\u00a73\u2725]";
         }
         if (Stars >= 3800 && Stars < 3900) {
             String[] digit = text.split("");
-            return "\u00a71" + digit[0] + "\u00a79" + digit[1] + "\u00a75" + digit[2] + digit[3] + "\u00a7d" + "\u2725";
+            return "\u00a71[" + digit[0] + "\u00a79" + digit[1] + "\u00a75" + digit[2] + digit[3] + "\u00a7d\u2725\u00a71]";
         }
         if (Stars >= 3900 && Stars < 4000) {
             String[] digit = text.split("");
-            return "\u00a7c" + digit[0] + "\u00a7a" + digit[1] + digit[2] + "\u00a73" + digit[3] + "\u00a79" + "\u2725";
+            return "\u00a7c[" + digit[0] + "\u00a7a" + digit[1] + digit[2] + "\u00a73" + digit[3] + "\u00a79\u2725]";
         }
         if (Stars >= 4000 && Stars < 4100) {
             String[] digit = text.split("");
-            return "\u00a75" + digit[0] + "\u00a7c" + digit[1] + digit[2] + "\u00a76" + digit[3] + "\u2725";
+            return "\u00a75[" + digit[0] + "\u00a7c" + digit[1] + digit[2] + "\u00a76" + digit[3] + "\u2725\u00a7e]";
         }
         if (Stars >= 4100 && Stars < 4200) {
             String[] digit = text.split("");
-            return "\u00a7e" + digit[0] + "\u00a76" + digit[1] + "\u00a7c" + digit[2] + "\u00a7d" + digit[3] + "\u2725";
+            return "\u00a7e[" + digit[0] + "\u00a76" + digit[1] + "\u00a7c" + digit[2] + "\u00a7d" + digit[3] + "\u2725\u00a75]";
         }
         if (Stars >= 4200 && Stars < 4300) {
             String[] digit = text.split("");
-            return "\u00a79" + digit[0] + "\u00a73" + digit[1] + "\u00a7b" + digit[2] + "\u00a7f" + digit[3] + "\u00a77" + "\u2725";
+            return "\u00a71[\u00a79" + digit[0] + "\u00a73" + digit[1] + "\u00a7b" + digit[2] + "\u00a7f" + digit[3] + "\u00a77\u2725]";
         }
         if (Stars >= 4300 && Stars < 4400) {
             String[] digit = text.split("");
-            return "\u00a75" + digit[0] + "\u00a78" + digit[1] + digit[2] + "\u00a75" + digit[3] + "\u2725";
+            return "\u00a70[\u00a75" + digit[0] + "\u00a78" + digit[1] + digit[2] + "\u00a75" + digit[3] + "\u2725\u00a70]";
         }
         if (Stars >= 4400 && Stars < 4500) {
             String[] digit = text.split("");
-            return "\u00a72" + digit[0] + "\u00a7a" + digit[1] + "\u00a7e" + digit[2] + "\u00a76" + digit[3] + "\u00a75" + "\u2725";
+            return "\u00a72[" + digit[0] + "\u00a7a" + digit[1] + "\u00a7e" + digit[2] + "\u00a76" + digit[3] + "\u00a75\u2725\u00a7d]";
         }
         if (Stars >= 4500 && Stars < 4600) {
             String[] digit = text.split("");
-            return "\u00a7f" + digit[0] + "\u00a7b" + digit[1] + digit[2] + "\u00a73" + digit[3] + "\u2725";
+            return "\u00a7f[" + digit[0] + "\u00a7b" + digit[1] + digit[2] + "\u00a73" + digit[3] + "\u2725]";
         }
         if (Stars >= 4600 && Stars < 4700) {
             String[] digit = text.split("");
-            return "\u00a7b" + digit[0] + "\u00a7e" + digit[1] + digit[2] + "\u00a76" + digit[3] + "\u00a7d" + "\u2725";
+            return "\u00a73[\u00a7b" + digit[0] + "\u00a7e" + digit[1] + digit[2] + "\u00a76" + digit[3] + "\u00a7d\u2725\u00a75]";
         }
         if (Stars >= 4700 && Stars < 4800) {
             String[] digit = text.split("");
-            return "\u00a74" + digit[0] + "\u00a7c" + digit[1] + digit[2] + "\u00a79" + digit[3] + "\u00a71" + "\u2725";
+            return "\u00a7f[\u00a74" + digit[0] + "\u00a7c" + digit[1] + digit[2] + "\u00a79" + digit[3] + "\u00a71\u2725\u00a79]";
         }
         if (Stars >= 4800 && Stars < 4900) {
             String[] digit = text.split("");
-            return "\u00a75" + digit[0] + "\u00a7c" + digit[1] + "\u00a76" + digit[2] + "\u00a7e" + digit[3] + "\u00a7b" + "\u2725";
+            return "\u00a75[" + digit[0] + "\u00a7c" + digit[1] + "\u00a76" + digit[2] + "\u00a7e" + digit[3] + "\u00a7b\u2725\u00a73]";
         }
         if (Stars >= 4900 && Stars < 5000) {
             String[] digit = text.split("");
-            return "\u00a7a" + digit[0] + "\u00a7f" + digit[1] + digit[2] + "\u00a7a" + digit[3] + "\u2725";
+            return "\u00a72[\u00a7a" + digit[0] + "\u00a7f" + digit[1] + digit[2] + "\u00a7a" + digit[3] + "\u2725\u00a72]";
         }
         if (Stars >= 5000) {
             String[] digit = text.split("");
-            return "\u00a74" + digit[0] + "\u00a75" + digit[1] + "\u00a79" + digit[2] + digit[3] + "\u00a71" + "\u2725";
+            return "\u00a74[" + digit[0] + "\u00a75" + digit[1] + "\u00a79" + digit[2] + digit[3] + "\u00a71\u2725\u00a70]";
         }
         return "NaN";
     }
@@ -1708,9 +1711,9 @@ public class Statsify {
                 sender.addChatMessage(new ChatComponentText("\u00a7r\u00a7bmade by\u00a7e melissalmao\u00a7r"));
                 sender.addChatMessage(new ChatComponentText(""));
                 sender.addChatMessage(new ChatComponentText("\u00a7r\u00a73Use /tabformat <number> to select a preset.\u00a7r"));
-                sender.addChatMessage(new ChatComponentText("\u00a7r        1) \u00a7dP \u00a77[\u00a76200\u272b\u00a77] \u00a7dFontaine\u30fb\u00a7f1.36\n" +
-                        "\u00a7r        2) \u00a7dP \u00a76200\u272b\u30fb\u00a7dFontaine\u30fb\u00a7f1.36\n" +
-                        "\u00a7r        3) \u00a7dP \u00a7dFontaine\u30fb\u00a7f1.36\n"));
+                sender.addChatMessage(new ChatComponentText("\u00a7r        1) \u00a7dP \u00a76[200\u272b] \u00a7dFontaine \u00a78| \u00a7a1.36\n" +
+                        "\u00a7r        2) \u00a7dP \u00a76200\u272b \u00a78| \u00a7dFontaine \u00a78| \u00a7a1.36\n" +
+                        "\u00a7r        3) \u00a7dP \u00a7dFontaine \u00a78| \u00a7a1.36\n"));
 
                 sender.addChatMessage(new ChatComponentText(""));
                 return;
